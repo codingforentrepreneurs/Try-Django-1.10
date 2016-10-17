c6d3a4daf25c606b7db850b995ee5b802da197ea
CFE <hello@teamcfe.com>
2016-10-17 16:50:30 -0700

    10 - Models

--- src/shortener/admin.py
+++ src/shortener/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import KirrURL
+
+
+admin.site.register(KirrURL)
--- src/shortener/models.py
+++ src/shortener/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class KirrURL(models.Model):
+    url = models.CharField(max_length=220, )
+
+    def __str__(self):
+        return str(self.url)
+
+    def __unicode__(self):
+        return str(self.url)
+
+
+
+'''
+python manage.py makemigrations
+python manage.py migrate
+
+python manage.py createsuperuser
+'''
