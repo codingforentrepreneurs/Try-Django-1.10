d89ddbcaf69e2e5713e65b3ef198d8235b0fe11b
CFE <hello@teamcfe.com>
2016-10-19 09:58:53 -0700

    12 - Handling Migration Failures

--- src/shortener/models.py
+++ src/shortener/models.py
@@ -3,8 +3,11 @@ from django.db import models
 # Create your models here.
 
 class KirrURL(models.Model):
-    url = models.CharField(max_length=220, )
-    shortcode = models.CharField(max_length=15, unique=True)
+    url         = models.CharField(max_length=220, )
+    shortcode   = models.CharField(max_length=15, unique=True)
+    updated     = models.DateTimeField(auto_now=True) #everytime the model is saved
+    timestamp   = models.DateTimeField(auto_now_add=True) #when model was created
+    #empty_datetime = models.DateTimeField(auto_now=False, auto_now_add=False)
     #shortcode = models.CharField(max_length=15, null=True) Empty in database is okay
     #shortcode = models.CharField(max_length=15, default='cfedefaultshortcode')
 
