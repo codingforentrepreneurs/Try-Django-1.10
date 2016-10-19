b475382d4ba8ef8147dcc34db35663c74eb8ba48
CFE <hello@teamcfe.com>
2016-10-19 09:50:53 -0700

    11 - Make Changes to Models

--- src/shortener/models.py
+++ src/shortener/models.py
@@ -4,6 +4,9 @@ from django.db import models
 
 class KirrURL(models.Model):
     url = models.CharField(max_length=220, )
+    shortcode = models.CharField(max_length=15, unique=True)
+    #shortcode = models.CharField(max_length=15, null=True) Empty in database is okay
+    #shortcode = models.CharField(max_length=15, default='cfedefaultshortcode')
 
     def __str__(self):
         return str(self.url)
