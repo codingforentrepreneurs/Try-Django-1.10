df51c7d72a0dd6f2000d29d75c7ae47c0e238902
CFE <hello@teamcfe.com>
2016-10-19 10:20:58 -0700

    14 - Override the Save Method

--- src/shortener/models.py
+++ src/shortener/models.py
@@ -1,6 +1,16 @@
+import random
+import string
 from django.db import models
 
 # Create your models here.
+
+def code_generator(size=6, chars=string.ascii_lowercase + string.digits):
+    # new_code = ''
+    # for _ in range(size):
+    #     new_code += random.choice(chars)
+    # return new_code
+    return ''.join(random.choice(chars) for _ in range(size))
+
 
 class KirrURL(models.Model):
     url         = models.CharField(max_length=220, )
@@ -10,6 +20,14 @@ class KirrURL(models.Model):
     #empty_datetime = models.DateTimeField(auto_now=False, auto_now_add=False)
     #shortcode = models.CharField(max_length=15, null=True) Empty in database is okay
     #shortcode = models.CharField(max_length=15, default='cfedefaultshortcode')
+
+    def save(self, *args, **kwargs):
+        print("something")
+        self.shortcode = code_generator()
+        super(KirrURL, self).save(*args, **kwargs)
+
+    # def my_save(self):
+    #     self.save()
 
     def __str__(self):
         return str(self.url)
