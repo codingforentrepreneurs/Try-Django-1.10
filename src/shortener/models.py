a80716e8a684a769bbb5a5816ba4c1e9b6cf7c74
CFE <hello@teamcfe.com>
2016-10-19 14:45:07 -0700

    16 - Model Manager and Refresh Shortcodes

--- src/shortener/models.py
+++ src/shortener/models.py
@@ -5,14 +5,35 @@ from django.db import models
 from .utils import code_generator, create_shortcode
 
 
+
+class KirrURLManager(models.Manager):
+    def all(self, *args, **kwargs):
+        qs_main = super(KirrURLManager, self).all(*args, **kwargs)
+        qs = qs_main.filter(active=True)
+        return qs
+
+    def refresh_shortcodes(self):
+        qs = KirrURL.objects.filter(id__gte=1)
+        new_codes = 0
+        for q in qs:
+            q.shortcode = create_shortcode(q)
+            print(q.shortcode)
+            q.save()
+            new_codes += 1
+        return "New codes made: {i}".format(i=new_codes)
+
+
 class KirrURL(models.Model):
     url         = models.CharField(max_length=220, )
-    shortcode   = models.CharField(max_length=15, unique=True, blank=True)
+    shortcode   = models.CharField(max_length=15, default='abc', unique=True, blank=True)
     updated     = models.DateTimeField(auto_now=True) #everytime the model is saved
     timestamp   = models.DateTimeField(auto_now_add=True) #when model was created
+    active      = models.BooleanField(default=True)
     #empty_datetime = models.DateTimeField(auto_now=False, auto_now_add=False)
     #shortcode = models.CharField(max_length=15, null=True) Empty in database is okay
     #shortcode = models.CharField(max_length=15, default='cfedefaultshortcode')
+    objects = KirrURLManager()
+    #some_random = KirrURLManager()
 
     def save(self, *args, **kwargs):
         if self.shortcode is None or self.shortcode == "":
--- src/shortener/utils.py
+++ src/shortener/utils.py
@@ -13,9 +13,6 @@ def code_generator(size=6, chars=string.ascii_lowercase + string.digits):
 
 def create_shortcode(instance, size=6):
     new_code = code_generator(size=size)
-    print(instance)
-    print(instance.__class__)
-    print(instance.__class__.__name__)
     Klass = instance.__class__
     qs_exists = Klass.objects.filter(shortcode=new_code).exists()
     if qs_exists:
