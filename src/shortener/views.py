089d1d31e70e95b3d7f1257f154e48a38747acd4
CFE <hello@teamcfe.com>
2016-10-20 15:42:29 -0700

    29 - Rendering New Templates and Context

--- src/shortener/views.py
+++ src/shortener/views.py
@@ -24,14 +24,24 @@ class HomeView(View):
 
     def post(self, request, *args, **kwargs):
         form = SubmitUrlForm(request.POST)
-        if form.is_valid():
-            print(form.cleaned_data.get("url"))
-
         context = {
             "title": "Kirr.co",
             "form": form
         }
-        return render(request, "shortener/home.html",context)
+        template = "shortener/home.html"
+        if form.is_valid():
+            new_url = form.cleaned_data.get("url")
+            obj, created = KirrURL.objects.get_or_create(url=new_url)
+            context = {
+                "object": obj,
+                "created": created,
+            }
+            if created:
+                template = "shortener/success.html"
+            else:
+                template = "shortener/already-exists.html"
+    
+        return render(request, template ,context)
 
 
 class KirrCBView(View): #class based view
