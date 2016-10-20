0d9c6fd90256224e3c40631c54d6e2d399e2f4c8
CFE <hello@teamcfe.com>
2016-10-20 09:42:07 -0700

    18 - CBV Views, FBV Views, and URLs

--- src/kirr/urls.py
+++ src/kirr/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from shortener.views import kirr_redirect_view, KirrCBView
+
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^new-admin/', admin.site.urls),
+    url(r'^view-1/$', kirr_redirect_view),
+    url(r'^view-2/$', KirrCBView.as_view()), #joincfe.com/projects/
 ]
--- src/shortener/views.py
+++ src/shortener/views.py
@@ -1,3 +1,13 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.views import View
 
 # Create your views here.
+def kirr_redirect_view(request, *args, **kwargs): #function based view FBV
+    return HttpResponse("hello")
+
+
+class KirrCBView(View): #class based view
+    def get(self, request, *args, **kwargs):
+        return HttpResponse("hello again")
+
