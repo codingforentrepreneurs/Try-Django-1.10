a7eda2691566cca6f0a62da2cd9e9c5f49c963c1
CFE <hello@teamcfe.com>
2016-10-20 14:10:55 -0700

    26 - HTML Form

--- src/shortener/views.py
+++ src/shortener/views.py
@@ -6,12 +6,24 @@ from .models import KirrURL
 
 # Create your views here.
 
+def home_view_fbv(request, *args, **kwargs):
+    if request.method == "POST":
+        print(request.POST)
+    return render(request, "shortener/home.html", {})
 
 
 class HomeView(View):
     def get(self, request, *args, **kwargs):
         return render(request, "shortener/home.html", {}) # Try Django 1.8 & 1.9 http://joincfe.com/youtube
 
+    def post(self, request, *args, **kwargs):
+        #some_dict = {}
+        #some_dict['url'] #error 
+        #some_dict.get('url', "http://joincfe.com") #None
+        print(request.POST)
+        print(request.POST["url"])
+        print(request.POST.get("url"))
+        return render(request, "shortener/home.html", {})
 
 
 class KirrCBView(View): #class based view
