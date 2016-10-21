ede729bd727232866dcadca25511f35799268748
CFE <hello@teamcfe.com>
2016-10-20 17:00:32 -0700

    32 - Integrate Bootstrap

--- src/shortener/forms.py
+++ src/shortener/forms.py
@@ -4,7 +4,16 @@ from django import forms
 from .validators import validate_url, validate_dot_com
 
 class SubmitUrlForm(forms.Form):
-    url = forms.CharField(label='Submit URL', validators=[validate_url, validate_dot_com])
+    url = forms.CharField(
+            label='', 
+            validators=[validate_url],
+            widget = forms.TextInput(
+                    attrs ={
+                        "placeholder": "Long URL",
+                        "class": "form-control"
+                        }
+                )
+            )
 
     # def clean(self):
     #     cleaned_data = super(SubmitUrlForm, self).clean()
